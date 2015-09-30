7dc0ad7eddc6dac20e35710f28aebbdb64fac178
Marcus Stong <stongo@gmail.com>
2015-09-30 14:49:46 -0400

    make example make sense

--- example/index.js
+++ example/index.js
@@ -1,6 +1,8 @@
+var NsquishyWorker = require('nsquishy-worker');
+
 var workerOptions = {
     nsquishyOptions: {
-        channel: 'example-worker'
+        channel: 'talky-trace-pg-worker'
     },
     writer: true,
     match: function (msg, msgBody) {
@@ -10,17 +12,29 @@ var workerOptions = {
         return false;
     },
     job: function (msg, msgBody, callback) {
-        var Message = require('nsquishy-message');
-        var message = new Message({
-            source: 'example-worker',
-            payload: 'cowabunga'
-        });
-        this.nsqWriter.publish('events', JSON.stringify(message));
-        return callback(null, 'complete');
+        // Do some stuff
+        // ...
+        var status = 'complete';
+        return callback(null, status);
     },
     finish: function (data, msg, msgBody, next) {
-        console.log('job status: %s', data);
+        var Message = require('nsquishy-message');
+        if (data === 'complete') {
+            var message = new Message({
+                source: 'example-worker',
+                payload: data
+            });
+            this.nsqWriter.publish('events', JSON.stringify(message));
+            return next();
+        }
+
+        var message = new Message({
+            source: 'example-worker',
+            payload: 'there was a problem'
+        });
+        this.nsqWriter.publish('events', JSON.stringify(message));
         return next();
+
     }
 };
 var NsquishyWorker = require('nsquishy-worker');
